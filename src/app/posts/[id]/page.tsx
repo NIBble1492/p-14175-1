bd3639c3b9b44d9eb1045f0a39aaed68043ef169
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-29 12:40:43 +0900

    feat: 글 상세페이지에서 삭제 기능 추가 및 수정 링크 연동

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,12 +2,27 @@
 
 import { apiFetch } from "@/lib/backend/client";
 import type { PostWithContentDto } from "@/type/post";
-import { useParams } from "next/navigation";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Page() {
-  const { id } = useParams<{ id: string }>();
+  const router = useRouter();
+
+  const { id: idStr } = useParams<{ id: string }>();
+  const id = Number(idStr);
+  
   const [post, setPost] = useState<PostWithContentDto | null>(null);
+
+  const deletePost = (id: number) => {
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+
+      router.replace("/posts");
+    });
+  };
 
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`)
@@ -20,11 +35,21 @@ export default function Page() {
     <>
       <h1>글 상세페이지</h1>
 
-      <>
-        <div>번호 : {post.id}</div>
-        <div>제목: {post.title}</div>
-        <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
-      </>
+      <div>번호 : {post.id}</div>
+      <div>제목: {post.title}</div>
+      <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
+
+      <div className="flex gap-2">
+        <button
+          className="p-2 rounded border cursor-pointer"
+          onClick={() => confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) && deletePost(post.id)}
+        >
+          삭제
+        </button>
+        <Link className="p-2 rounded border" href={`/posts/${post.id}/edit`}>
+          수정
+        </Link>
+      </div>
     </>
   );
 }
