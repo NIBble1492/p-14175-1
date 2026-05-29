20ec01e2cc14eb2b17758aff2662b72886ff5b09
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-29 14:42:57 +0900

    feat: 글 상세페이지 댓글 목록 조회 기능 추가 및 라우트 파라미터 반환 방식 수정

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,32 +1,31 @@
 "use client";
 
 import { apiFetch } from "@/lib/backend/client";
-import type { PostWithContentDto } from "@/type/post";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import Link from "next/link";
-import { useParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
-export default function Page() {
+export default function Page({ params }: { params: Promise<{ id: number }> }) {
+  const { id } = use(params);
   const router = useRouter();
 
-  const { id: idStr } = useParams<{ id: string }>();
-  const id = Number(idStr);
-  
   const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>([]);
 
   const deletePost = (id: number) => {
     apiFetch(`/api/v1/posts/${id}`, {
       method: "DELETE",
     }).then((data) => {
       alert(data.msg);
-
       router.replace("/posts");
     });
   };
 
   useEffect(() => {
-    apiFetch(`/api/v1/posts/${id}`)
-      .then(setPost);
+    apiFetch(`/api/v1/posts/${id}`).then(setPost);
+
+    apiFetch(`/api/v1/posts/${id}/comments`).then(setPostComments);
   }, []);
 
   if (post == null) return <div>로딩중...</div>;
@@ -41,8 +40,11 @@ export default function Page() {
 
       <div className="flex gap-2">
         <button
-          className="p-2 rounded border cursor-pointer"
-          onClick={() => confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) && deletePost(post.id)}
+          className="p-2 rounded border"
+          onClick={() =>
+            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
+            deletePost(post.id)
+          }
         >
           삭제
         </button>
@@ -50,6 +52,22 @@ export default function Page() {
           수정
         </Link>
       </div>
+
+      <h2>댓글 목록</h2>
+
+      {postComments == null && <div>댓글 로딩중...</div>}
+
+      {postComments != null && postComments.length == 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
+
+      {postComments != null && postComments.length > 0 && (
+        <ul>
+          {postComments.map((comment) => (
+            <li key={comment.id}>{comment.content}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
--- src/type/post.ts
+++ src/type/post.ts
@@ -1,8 +1,18 @@
 export type PostDto = {
   id: number;
+  createDate: string;
+  modifyDate: string;
   title: string;
+  
 };
 
 export type PostWithContentDto = PostDto & {
+  content: string;
+};
+
+export type PostCommentDto = {
+  id: number;
+  createDate: string;
+  modifyDate: string;
   content: string;
 };
